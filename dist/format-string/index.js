62227000760e9fdcc5d028cf3e74822c1350247e
Sameer Khan <sameerkhanofficial@gmail.com>
2022-02-28 12:09:43 +0530

    feat: updated code for npm, updated code coverage

--- dist/format-string/index.js
+++ dist/format-string/index.js
@@ -12,10 +12,17 @@ function formatString(text, replacements, start, end) {
     }
     if (start === end)
         return text;
+    if (!start.replace(/\s/g, "").length || !end.replace(/\s/g, "").length) {
+        throw new Error("You cannot only use whitespace as a separator. Please include a character for specifying separators better.");
+    }
     var startRegex = (0, escape_regexp_component_1.default)(start);
     var endRegex = (0, escape_regexp_component_1.default)(end);
     for (var arg in replacements) {
-        text = text.replace(new RegExp(startRegex + arg + endRegex, "gi"), replacements[arg].toString());
+        if (typeof replacements[arg] === "string" ||
+            typeof replacements[arg] === "boolean" ||
+            typeof replacements[arg] === "number") {
+            text = text.replace(new RegExp(startRegex + arg + endRegex, "gi"), replacements[arg].toString());
+        }
     }
     return text;
 }
